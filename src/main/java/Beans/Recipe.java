9a2fb8d9016ab6b16066b233f023bf1876fd7e1f
Jamie <waffleman566@gmail.com>
2019-03-02 14:27:32 -0500

    Fleshed skeleton out with basic fields (map of ingredients/quantities, list of instructions, and fields for serving size/total servings) and added getters and setters.

--- src/main/java/Beans/Recipe.java
+++ src/main/java/Beans/Recipe.java
@@ -5,6 +5,9 @@
  */
 package Beans;
 
+import java.util.AbstractMap.SimpleEntry;
+import java.util.ArrayList;
+
 /**
  *
  * @author soup
@@ -12,4 +15,50 @@ package Beans;
 public class Recipe {
     //Should contain a list of key-value pairs detailing the ingredients required and how many of each necessary (both saved as Strings to maintain flexibility)
     //Should also have a list of the instructions for the recipe; each step is a separate object in the list
+    
+    SimpleEntry<Ingredient, String> ingredients;
+    ArrayList<String> instructions;
+    String servingSize;
+    float totalServings;
+    
+    public Recipe() { }
+    
+    public Recipe(SimpleEntry<Ingredient, String> ingredients, ArrayList<String> instructions, String servingSize, float totalServings) {
+        this.ingredients = ingredients;
+        this.instructions = instructions;
+        this.servingSize = servingSize;
+        this.totalServings = totalServings;
+    }
+
+    public SimpleEntry<Ingredient, String> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(SimpleEntry<Ingredient, String> ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public ArrayList<String> getInstructions() {
+        return instructions;
+    }
+
+    public void setInstructions(ArrayList<String> instructions) {
+        this.instructions = instructions;
+    }
+
+    public String getServingSize() {
+        return servingSize;
+    }
+
+    public void setServingSize(String servingSize) {
+        this.servingSize = servingSize;
+    }
+
+    public float getTotalServings() {
+        return totalServings;
+    }
+
+    public void setTotalServings(float totalServings) {
+        this.totalServings = totalServings;
+    }
 }
