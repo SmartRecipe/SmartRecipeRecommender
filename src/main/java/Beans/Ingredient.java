b93c8f336a79872ab7b04722c007ec9bd1317995
Jamie <waffleman566@gmail.com>
2019-03-02 14:54:31 -0500

    Fleshed out Ingredient skeleton with fields and getters/setters; added name, description, and nutrition value fields to Recipe.

--- src/main/java/Beans/Ingredient.java
+++ src/main/java/Beans/Ingredient.java
@@ -11,4 +11,35 @@ package Beans;
  */
 public class Ingredient {
     //Should have fields pertaining to nutrition values, dietary implications, etc. Can all be pulled from Edamam API
+    //Nutritional value ("nutVal") is represented as a String for now, but may want to consider creating class
+    //to represent it as its own object, as it will have a number of different values within (calories, sugars, vitamins,
+    //etc.).
+    
+    String name;
+    String nutVal; //Nutritional value per gram (many ways to measure ingredients, so bare mass might be most consistent)
+    
+    public Ingredient() { }
+    
+    public Ingredient(String name, String nutVal) {
+        this.name = name;
+        this.nutVal = nutVal;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNutVal() {
+        return nutVal;
+    }
+
+    public void setNutVal(String nutVal) {
+        this.nutVal = nutVal;
+    }
+    
+    
 }
--- src/main/java/Beans/Recipe.java
+++ src/main/java/Beans/Recipe.java
@@ -13,9 +13,15 @@ import java.util.ArrayList;
  * @author soup
  */
 public class Recipe {
-    //Should contain a list of key-value pairs detailing the ingredients required and how many of each necessary (both saved as Strings to maintain flexibility)
-    //Should also have a list of the instructions for the recipe; each step is a separate object in the list
+    //Should contain a list of key-value pairs detailing the ingredients required and how many of each necessary
+    //(both saved as Strings to maintain flexibility) Should also have a list of the instructions for the recipe;
+    //each step is a separate object in the list Nutritional value ("nutVal") is represented as a String for now,
+    //but may want to consider creating class to represent it as its own object, as it will have a number of different
+    //values within (calories, sugars, vitamins, etc.).
     
+    String name;
+    String desc;
+    String nutVal; //Nutritional value per serving
     SimpleEntry<Ingredient, String> ingredients;
     ArrayList<String> instructions;
     String servingSize;
@@ -23,11 +29,39 @@ public class Recipe {
     
     public Recipe() { }
     
-    public Recipe(SimpleEntry<Ingredient, String> ingredients, ArrayList<String> instructions, String servingSize, float totalServings) {
+    public Recipe(String name, String desc, String nutVal, SimpleEntry<Ingredient, String> ingredients,
+            ArrayList<String> instructions, String servingSize, float totalServings) {
+        this.name = name;
+        this.desc = desc;
+        this.nutVal = nutVal;
         this.ingredients = ingredients;
         this.instructions = instructions;
         this.servingSize = servingSize;
         this.totalServings = totalServings;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public String getDesc() {
+        return desc;
+    }
+    
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+    
+    public String getNutVal() {
+        return nutVal;
+    }
+    
+    public void setNutVal(String nutVal) {
+        this.nutVal = nutVal;
     }
 
     public SimpleEntry<Ingredient, String> getIngredients() {
