f77cdd0a6cf4062800ddc0552c50b6b632d6e21c
Pranav Gaikwad <gaikwad.pranav@gmail.com>
2019-03-09 19:58:27 -0500

    removed menu item containers

--- Frontend/src/containers/app/app.js
+++ Frontend/src/containers/app/app.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
-import SideNav from '@trendmicro/react-sidenav';
+import SideNav, { NavItem, NavIcon, NavText } from '@trendmicro/react-sidenav';
 import { Route, Router } from 'react-router-dom';
 
 import '@trendmicro/react-sidenav/dist/react-sidenav.css';
 
-import MenuItem from '../../components/menu/menu-item.component';
 import { history, menuItemProps } from '../../utils/app.constants';
+
+import RecipesPageContainer from '../recipes-page/recipes-page.component';
+import IngredientsPageContainer from '../ingredients-page/ingredients-page.component';
 
 class App extends Component {
   render() {
@@ -20,17 +22,33 @@ class App extends Component {
                                 if (location.pathname !== to) {
                                     history.push(to);
                                 }
+                                console.log('Triggered change');
+                                console.log(location);
                             }}>
                             <SideNav.Toggle />
-                            <SideNav.Nav defaultSelected="home">
-                                <MenuItem title={menuItemProps.recipesMenu.title}/>
-                                <MenuItem title={menuItemProps.ingredientsMenu.title}/>
+                            <SideNav.Nav defaultSelected={menuItemProps.recipesMenu.key}>
+                                <NavItem eventKey={menuItemProps.recipesMenu.key}>
+                                    <NavIcon>
+                                        <i className="fa fa-fw fa-home" style={{ fontSize: '1.75em' }} />
+                                    </NavIcon>
+                                    <NavText>
+                                        {menuItemProps.recipesMenu.title}
+                                    </NavText>
+                                </NavItem>
+                                <NavItem eventKey={menuItemProps.ingredientsMenu.key}>
+                                    <NavIcon>
+                                        <i className="fa fa-fw fa-home" style={{ fontSize: '1.75em' }} />
+                                    </NavIcon>
+                                    <NavText>
+                                        {menuItemProps.ingredientsMenu.title}
+                                    </NavText>
+                                </NavItem>
                             </SideNav.Nav>
                         </SideNav>
                         <main>
                             <Route path="/" exact/>
-                            <Route path={menuItemProps.recipesMenu.route}/>
-                            <Route path={menuItemProps.ingredientsMenu.route}/>
+                            <Route path={menuItemProps.recipesMenu.route} component={props => <RecipesPageContainer/>}/>
+                            <Route path={menuItemProps.ingredientsMenu.route} component={props => <IngredientsPageContainer/>}/>
                         </main>
                     </React.Fragment>)}
                 />
