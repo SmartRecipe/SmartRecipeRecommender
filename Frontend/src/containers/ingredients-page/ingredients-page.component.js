5d13f4925cec0faa78b1ed4e7dd8ed4c08859293
Pranav Gaikwad <gaikwad.pranav@gmail.com>
2019-03-09 19:59:04 -0500

    added containers for recipes and ingredients pages

---
+++ Frontend/src/containers/ingredients-page/ingredients-page.component.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class IngredientsPageContainer extends Component {
+  render() {
+    return (
+        <div>
+            <h1>Ingredients</h1>
+        </div>
+    );
+  }
+}
+
+export default IngredientsPageContainer;
---
+++ Frontend/src/containers/recipes-page/recipes-page.component.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class RecipesPageContainer extends Component {
+  render() {
+    return (
+        <div>
+            <h1>Recipes</h1>
+        </div>
+    );
+  }
+}
+
+export default RecipesPageContainer;
