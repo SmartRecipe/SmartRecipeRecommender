4ffeb3853d6568c2bc0bbcd49651c5293db0e0af
Pranav Gaikwad <gaikwad.pranav@gmail.com>
2019-03-09 20:40:39 -0500

    added card to recipes page component

--- Frontend/src/containers/recipes-page/recipes-page.component.js
+++ Frontend/src/containers/recipes-page/recipes-page.component.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 
+import RecipeCardComponent from '../../components/cards/recipe-card.component'
+
 class RecipesPageContainer extends Component {
   render() {
     return (
         <div>
-            <h1>Recipes</h1>
+            <RecipeCardComponent />
         </div>
     );
   }
