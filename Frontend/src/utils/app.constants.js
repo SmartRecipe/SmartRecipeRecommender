91b3a81ff4f7e7eec958482e0aebe5194420a10d
Pranav Gaikwad <gaikwad.pranav@gmail.com>
2019-03-11 16:01:16 -0400

    added constants for sign in related actions

--- Frontend/src/utils/app.constants.js
+++ Frontend/src/utils/app.constants.js
@@ -2,6 +2,10 @@ import createHistory from 'history/createBrowserHistory'; // eslint-disable-line
 
 export const history = createHistory();
 
+export const appConstants = {
+    appTitle: "Smart Recipe Recommender",
+};
+
 // properties of items in the navigation menu
 export const menuItemProps = {
     recipesMenu : {
@@ -17,3 +21,18 @@ export const menuItemProps = {
         route: "/ingredients",
     },
 }
+
+// common constants related to API actions
+export const statusRequests = {
+    pending: "REQUEST_PENDING",
+    success: "REQUEST_SUCCESS",
+    fail: "REQUEST_FAIL",
+};
+
+// constants related to user sign in actions
+export const actionsSignIn = {
+    failed: "ACTION_SIGN_IN_FAILED",
+    pending: "ACTION_SIGN_IN_PENDING",
+    success: "ACTION_SIGN_IN_SUCCESS",
+    signout: "ACTION_SIGN_OUT",
+};
