774d8b46ffe4e02615a74194e5c4745bd617a778
Pranav Gaikwad <gaikwad.pranav@gmail.com>
2019-03-11 16:01:05 -0400

    added thunk middleware

--- Frontend/src/store/configureStore.js
+++ Frontend/src/store/configureStore.js
@@ -1,11 +1,14 @@
-import { createStore as createReduxStore } from 'redux';
+import thunk from 'redux-thunk';
+import { createStore as createReduxStore, applyMiddleware } from 'redux';
 
 import rootReducer from './reducers';
 
 const configureStore = (initialState = {}) => {
+  const middleware = [thunk];
   const store = createReduxStore(
     rootReducer,
     initialState,
+    applyMiddleware(...middleware),
   );
 
   return store;
