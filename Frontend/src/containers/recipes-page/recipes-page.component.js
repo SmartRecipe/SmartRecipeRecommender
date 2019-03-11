ce43564b74d07fae91d817af1beb4894f67e8a4d
Pranav Gaikwad <gaikwad.pranav@gmail.com>
2019-03-11 16:00:18 -0400

    jslint fixes

--- Frontend/src/containers/recipes-page/recipes-page.component.js
+++ Frontend/src/containers/recipes-page/recipes-page.component.js
@@ -1,22 +1,60 @@
+import PropTypes from 'prop-types';
 import React, { Component } from 'react';
-import { CardDeck } from 'react-bootstrap';
+import { withStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+// import Paper from '@material-ui/core/Paper';
 
-import RecipeCardComponent from '../../components/cards/recipe-card.component'
+import RecipeCardComponent2 from '../../components/cards/card.component';
+// import RecipeCardComponent from '../../components/cards/recipe-card.component';
 
 /**
  * Container for recipes page
  */
 class RecipesPageContainer extends Component {
   render() {
+    // const { classes } = this.props;
+
     return (
-        <div>
-            <CardDeck className="card-deck-container">
-                <RecipeCardComponent title="Sample Recipe 1"/>
-                <RecipeCardComponent title="Sample Recipe 2"/>
-            </CardDeck>
+        <div className="card-deck-container">
+          <Grid container spacing={24}>
+            <Grid item sm>
+              <RecipeCardComponent2 title="Sample Recipe 1" description="Sample Description 1"/>
+            </Grid>
+            <Grid item sm>
+              <RecipeCardComponent2 title="Sample Recipe 2" description="Sample Description 1"/>
+            </Grid>
+            <Grid item sm> 
+              <RecipeCardComponent2 title="Sample Recipe 2" description="Sample Description 1"/>
+            </Grid>
+          </Grid>
         </div>
     );
   }
 }
 
-export default RecipesPageContainer;
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing.unit * 2,
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+});
+
+RecipesPageContainer.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(RecipesPageContainer);
+
+
+/**
+ * <div>
+            <CardDeck className="card-deck-container">
+                <RecipeCardComponent title="Sample Recipe 1"/>
+                <RecipeCardComponent title="Sample Recipe 2"/>
+            </CardDeck>
+        </div>
+ */
