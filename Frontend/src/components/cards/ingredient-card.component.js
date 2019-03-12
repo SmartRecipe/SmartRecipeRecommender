cff4b4c2e19795908bb2a6245109f9eeeb19f223
Pranav Gaikwad <gaikwad.pranav@gmail.com>
2019-03-12 19:20:41 -0400

    added card component to show ingredient information

---
+++ Frontend/src/components/cards/ingredient-card.component.js
@@ -0,0 +1,78 @@
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+import Card from '@material-ui/core/Card';
+import Button from '@material-ui/core/Button';
+import { withStyles } from '@material-ui/core/styles';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+
+
+import EditIcon from '@material-ui/icons/Edit';
+import ShareIcon from '@material-ui/icons/Share';
+import DeleteIcon from '@material-ui/icons/Delete';
+/**
+ * Generic card component to show an ingredient
+ */
+class IngredientCardComponent extends Component {
+  render(){
+
+    const { 
+      title, 
+      classes,
+      description, 
+    } = this.props;
+    
+    return (
+      <Card className={classes.card}>
+        <CardContent>
+          <Typography variant="h6" component="h2">
+            Ingredient        
+          </Typography>
+          <Typography component="p">
+            Small Description
+          </Typography>
+        </CardContent>
+
+        <CardActions classes={classes.actions} disableActionSpacing>
+          <IconButton aria-label="Edit">
+            <EditIcon />
+          </IconButton>
+          <IconButton aria-label="Delete">
+            <DeleteIcon />
+          </IconButton>
+        </CardActions>
+      </Card>
+    );
+  }
+}
+
+const styles = {
+  card: {
+    minWidth: 180,
+    margin: 10,
+  },
+  bullet: {
+    display: 'inline-block',
+    margin: '0 2px',
+    transform: 'scale(0.8)',
+  },
+  title: {
+    fontSize: 14,
+  },
+  actions: {
+    display: 'flex',
+  },
+  pos: {
+    marginBottom: 12,
+  },
+};
+
+IngredientCardComponent.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(IngredientCardComponent);
+
--- Frontend/src/containers/ingredients-page/ingredients-page.component.js
+++ Frontend/src/containers/ingredients-page/ingredients-page.component.js
@@ -1,16 +1,94 @@
+import PropTypes from 'prop-types';
 import React, { Component } from 'react';
+import Fab from '@material-ui/core/Fab';
+import Grid from '@material-ui/core/Grid';
+import { withStyles } from '@material-ui/core/styles';
+
+import AddIcon from '@material-ui/icons/Add';
+
+import IngredientCardComponent from '../../components/cards/ingredient-card.component';
 
 /**
  * Main container for ingredients page
  */
 class IngredientsPageContainer extends Component {
+  constructor(props){
+    super(props);
+
+    this.onAddNewIngredient = this.onAddNewIngredient.bind(this);
+  }
+
+  onAddNewIngredient() {
+
+  }
+
   render() {
+    const { classes } = this.props;
+
     return (
-        <div>
-            <h1>Ingredients</h1>
+        <div className="card-deck-container">
+          <Fab onClick={this.onAddNewIngredient} color="primary" aria-label="Add" className={classes.fab}>
+            <AddIcon />
+          </Fab>
+          <Grid container spacing={8}>
+            <Grid item sm>
+              <IngredientCardComponent/>
+            </Grid>
+            <Grid item sm>
+              <IngredientCardComponent/>
+            </Grid>
+            <Grid item sm> 
+              <IngredientCardComponent/>
+            </Grid>
+            <Grid item sm> 
+              <IngredientCardComponent/>
+            </Grid>
+            <Grid item sm> 
+              <IngredientCardComponent/>
+            </Grid>
+          </Grid>
+          <Grid container spacing={8}>
+            <Grid item sm>
+              <IngredientCardComponent/>
+            </Grid>
+            <Grid item sm>
+              <IngredientCardComponent/>
+            </Grid>
+            <Grid item sm> 
+              <IngredientCardComponent/>
+            </Grid>
+            <Grid item sm> 
+              <IngredientCardComponent/>
+            </Grid>
+            <Grid item sm> 
+              <IngredientCardComponent/>
+            </Grid>
+          </Grid>
         </div>
     );
   }
 }
 
-export default IngredientsPageContainer;
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing.unit * 2,
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+  fab: {
+    margin: theme.spacing.unit,
+    position: 'fixed',
+    bottom: 0,
+    right: 0,
+    margin: 24,
+  },
+});
+
+IngredientsPageContainer.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(IngredientsPageContainer);
