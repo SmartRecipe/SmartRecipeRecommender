b85baef7cc03690425706f499d9efb9da164f589
Pranav Gaikwad <gaikwad.pranav@gmail.com>
2019-03-12 17:52:25 -0400

    added persist store to be used as the main reducer

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
-import CssBaseline from "@material-ui/core/CssBaseline";
 import indigo from '@material-ui/core/colors/indigo';
+import CssBaseline from "@material-ui/core/CssBaseline";
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+
+import { persistStore } from 'redux-persist';
+import { PersistGate } from 'redux-persist/integration/react'
 
 import App from './containers/app/app';
 import configureStore from './store/configureStore';
 import initialState from './reducers/auth/initial.state';
 
 const store = configureStore(initialState);
+
+const persistor = persistStore(store);
 
 const theme = createMuiTheme({
   palette: {
@@ -21,13 +26,14 @@ const theme = createMuiTheme({
   },
 });
 
-
 render(
     <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
         <MuiThemeProvider theme={theme}>
             <CssBaseline/>
             <App/>
         </MuiThemeProvider>
+      </PersistGate>
     </Provider>
   , document.getElementById('root')
 );
