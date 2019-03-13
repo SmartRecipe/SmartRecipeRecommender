35ed869105f4a596a80445a3c31f93b8f8190fc7
Pranav Gaikwad <gaikwad.pranav@gmail.com>
2019-03-13 17:31:52 -0400

    added default schema representations in initial state of ingredients' store

--- Frontend/src/reducers/ingredients-page/initial.state.js
+++ Frontend/src/reducers/ingredients-page/initial.state.js
@@ -1,3 +1,13 @@
+/**
+ * List of ingredients saved in the state.
+ * Each ingredient looks like this - 
+ * {
+ *     id : '',
+ *     name : '',
+ *     qty : '',
+ *     unit: '',
+ * }
+ */
 export default {
     ingredients: [],
 };
