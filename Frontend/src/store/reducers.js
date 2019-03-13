798d6d85cc57a13ee954fe73075ed6fb42c8c2d2
Pranav Gaikwad <gaikwad.pranav@gmail.com>
2019-03-13 17:32:01 -0400

    added recipesReducer

--- Frontend/src/store/reducers.js
+++ Frontend/src/store/reducers.js
@@ -2,6 +2,8 @@ import { combineReducers } from 'redux';
 
 import authReducer from '../reducers/auth/auth.reducer';
 import navigationReducer from '../reducers/navigation/navigation.reducer';
+import recipesReducer from '../reducers/recipes-page/recipes-page.reducer';
+import ingredientsReducer from '../reducers/ingredients-page/ingredients-page.reducer';
 
 /**
  * Combines all reducers into one big reducer
@@ -9,7 +11,9 @@ import navigationReducer from '../reducers/navigation/navigation.reducer';
  */
 const rootReducer = combineReducers({
   authReducer,
+  recipesReducer,
   navigationReducer,
+  ingredientsReducer,
 });
 
 export default rootReducer;
