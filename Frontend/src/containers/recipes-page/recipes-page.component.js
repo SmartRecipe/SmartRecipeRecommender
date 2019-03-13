444141abd6d92dd8edbda7d4ad0aa4a356548135
Pranav Gaikwad <gaikwad.pranav@gmail.com>
2019-03-13 17:31:17 -0400

    added dynamically created recipes on Recipes landing page, added create new recipe dialog

--- Frontend/src/containers/recipes-page/recipes-page.component.js
+++ Frontend/src/containers/recipes-page/recipes-page.component.js
@@ -1,4 +1,5 @@
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import Fab from '@material-ui/core/Fab';
 import React, { Component } from 'react';
 import Grid from '@material-ui/core/Grid';
@@ -6,7 +7,12 @@ import { withStyles } from '@material-ui/core/styles';
 
 import AddIcon from '@material-ui/icons/Add';
 
+import { addRecipe } from '../../actions/recipes-page/recipes-page.actions';
+
 import RecipeCardComponent from '../../components/cards/recipe-card.component';
+import AddRecipeDialog from '../../components/dialogs/add-recipe-dialog.component';
+
+const GRID_ROW_SIZE = 4;
 
 /**
  * Container for recipes page
@@ -15,43 +21,86 @@ class RecipesPageContainer extends Component {
   constructor(props) {
     super(props);
 
+    this.state = {
+      showDialog: false,
+    };
+
+    this.onDialogClosed = this.onDialogClosed.bind(this);
+    this.onDialogSubmit = this.onDialogSubmit.bind(this);
     this.onAddButtonClicked = this.onAddButtonClicked.bind(this);
   }
 
   onAddButtonClicked() {
-    
+    this.setState({
+      showDialog: true,
+    });
+  }
+
+  onDialogClosed() {
+    this.setState({
+      showDialog: false,
+    });
+  }
+
+  onDialogSubmit(e, recipe) {
+    this.onDialogClosed();
+
+    this.props.addRecipe(recipe);
+  }
+
+  getRecipesGrid(recipes) {
+    const totalRecipes = recipes.length;
+
+    const totalRows = totalRecipes / GRID_ROW_SIZE;
+
+    let rows = []
+
+    for (var i = 0; i < totalRows; i++) {
+      const start = i*GRID_ROW_SIZE;
+      let end = (i+1)*GRID_ROW_SIZE;
+      end = (totalRecipes < end) ? totalRecipes : end; 
+      let cols = []
+      for (var j = i*GRID_ROW_SIZE; j < end; j++) {
+        const currentRecipe = recipes[j];
+        if ((currentRecipe !== undefined) && (currentRecipe !== null)) {
+          const { id, name, short_description, description } = currentRecipe;
+          cols.push(
+            <Grid key={"recipe_item_" + j} item sm>
+              <RecipeCardComponent
+                key={id}
+                title={name}
+                shortDescription={short_description}
+                description={description}
+              />
+            </Grid>);
+        }
+      }
+      rows.push(<Grid key={"ing_row_" + i} container spacing={8}>{cols}</Grid>);
+    }
+
+    return rows;
   }
 
   render() {
-    const { classes } = this.props;
+    const { showDialog } = this.state;
+
+    const { classes, recipes } = this.props;
+
+    let recipesGrid = []
+
+    if (recipes !== undefined && recipes !== null) recipesGrid = this.getRecipesGrid(recipes);
 
     return (
         <div className="card-deck-container">
-          <Fab color="primary" aria-label="Add" className={classes.fab}>
+          <Fab color="primary" aria-label="Add" className={classes.fab} onClick={this.onAddButtonClicked}>
             <AddIcon />
           </Fab>
-          <Grid container spacing={18}>
-            <Grid item sm>
-              <RecipeCardComponent/>
-            </Grid>
-            <Grid item sm>
-              <RecipeCardComponent/>
-            </Grid>
-            <Grid item sm> 
-              <RecipeCardComponent/>
-            </Grid>
-          </Grid>
-          <Grid container spacing={18}>
-            <Grid item sm>
-              <RecipeCardComponent/>
-            </Grid>
-            <Grid item sm>
-              <RecipeCardComponent/>
-            </Grid>
-            <Grid item sm> 
-              <RecipeCardComponent/>
-            </Grid>
-          </Grid>
+          {
+            <AddRecipeDialog open={showDialog} onClose={this.onDialogClosed} onSubmit={this.onDialogSubmit} onFormChange={this.onFormChange}/>
+          }
+          {
+            recipesGrid
+          }
         </div>
     );
   }
@@ -79,19 +128,15 @@ RecipesPageContainer.propTypes = {
   classes: PropTypes.object.isRequired,
 };
 
-export default withStyles(styles)(RecipesPageContainer);
+const mapStateToProps = state => ({
+  recipes: state.recipesReducer.recipes,
+  currentRoute: state.navigationReducer.currentRoute,
+});
 
 
-/**
-          <Grid container spacing={12}>
-            <Grid item sm>
-              <RecipeCardComponent2 title="Sample Recipe 1" description="Sample Description 1"/>
-            </Grid>
-            <Grid item sm>
-              <RecipeCardComponent2 title="Sample Recipe 2" description="Sample Description 1"/>
-            </Grid>
-            <Grid item sm> 
-              <RecipeCardComponent2 title="Sample Recipe 2" description="Sample Description 1"/>
-            </Grid>
-          </Grid>
- */
+const mapDispatchToProps = dispatch => ({
+  addRecipe: (recipe) => dispatch(addRecipe(recipe)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(withStyles(styles)(RecipesPageContainer));
+
