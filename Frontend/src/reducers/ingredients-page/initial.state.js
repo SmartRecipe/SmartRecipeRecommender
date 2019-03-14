048bd2694c53030cde6216fc1d47f9591778e1eb
Pranav Gaikwad <gaikwad.pranav@gmail.com>
2019-03-14 02:53:19 -0400

    added sample ingredients

--- Frontend/src/reducers/ingredients-page/initial.state.js
+++ Frontend/src/reducers/ingredients-page/initial.state.js
@@ -9,5 +9,84 @@
  * }
  */
 export default {
-    ingredients: [],
+    ingredients: [
+        {
+            id: 1,
+            name: 'Whole Wheat',
+            qty: 1,
+            unit: 'lbs.',
+        },
+        {
+            id: 2,
+            name: 'Milk',
+            qty: 128,
+            unit: 'oz.',
+        },
+        {
+            id: 3,
+            name: 'Bacon',
+            qty: 1,
+            unit: 'lbs.',
+        },
+        {
+            id: 4,
+            name: 'Spinach',
+            qty: 1,
+            unit: 'lbs.',
+        },
+        {
+            id: 5,
+            name: 'Butter',
+            qty: 8,
+            unit: 'oz.',
+        },
+        {
+            id: 6,
+            name: 'Canola Oil',
+            qty: 24,
+            unit: 'oz.',
+        },
+        {
+            id: 7,
+            name: 'Baking Powder',
+            qty: 0.5,
+            unit: 'lbs.',
+        },
+        {
+            id: 8,
+            name: 'Smoked Turkey',
+            qty: 1,
+            unit: 'lbs.',
+        },
+        {
+            id: 9,
+            name: 'Ground Coffee',
+            qty: 1,
+            unit: 'lb.',
+        },
+        {
+            id: 10,
+            name: 'Salt',
+            qty: 2,
+            unit: 'lbs.',
+        },
+        {
+            id: 11,
+            name: 'Orange Juice',
+            qty: 24,
+            unit: 'oz.',
+        },
+        {
+            id: 12,
+            name: 'Honey',
+            qty: 8,
+            unit: 'oz.',
+        },
+        {
+            id: 13,
+            name: 'Taco Shells',
+            qty: 6,
+            unit: 'pc.',
+        },
+    ],
 };
