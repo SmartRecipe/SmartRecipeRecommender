3dbb60d8b6d8cd2b529b5a9e4c4f7b58b145c123
Pranav Gaikwad <gaikwad.pranav@gmail.com>
2019-03-14 02:53:26 -0400

    added sample recipes

--- Frontend/src/reducers/recipes-page/initial.state.js
+++ Frontend/src/reducers/recipes-page/initial.state.js
@@ -8,6 +8,44 @@
  *     description : '',
  * }
  */
+import { sampleRecipe } from '../../utils/app.constants';
+
 export default {
-    recipes: [],
+    recipes: [
+        {
+            id: 1,
+            name: sampleRecipe.name,
+            short_description: sampleRecipe.shortDescription,
+            description: sampleRecipe.description,
+            ingredients: [],
+        },
+        {
+            id: 2,
+            name: sampleRecipe.name,
+            short_description: sampleRecipe.shortDescription,
+            description: sampleRecipe.description,
+            ingredients: [],
+        },
+        {
+            id: 3,
+            name: sampleRecipe.name,
+            short_description: sampleRecipe.shortDescription,
+            description: sampleRecipe.description,
+            ingredients: [],
+        },
+        {
+            id: 4,
+            name: sampleRecipe.name,
+            short_description: sampleRecipe.shortDescription,
+            description: sampleRecipe.description,
+            ingredients: [],
+        },
+        {
+            id: 5,
+            name: sampleRecipe.name,
+            short_description: sampleRecipe.shortDescription,
+            description: sampleRecipe.description,
+            ingredients: [],
+        },
+    ],
 };
