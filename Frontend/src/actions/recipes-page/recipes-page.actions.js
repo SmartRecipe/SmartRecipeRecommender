c3b45106749f026c6f2621a69a29f4e073026d3d
Pranav Gaikwad <gaikwad.pranav@gmail.com>
2019-03-13 21:04:23 -0400

    minor fixes

--- Frontend/src/actions/recipes-page/recipes-page.actions.js
+++ Frontend/src/actions/recipes-page/recipes-page.actions.js
@@ -3,8 +3,8 @@ import uuidv1 from 'uuid/v1';
 import { actionsRecipes } from '../../utils/app.constants';
 
 /**
- * Populates state information when user navigates to another page
- * @param  {string} route    Route to navigate to
+ * Adding new recipe to the redux store
+ * @param  {Object} recipe    Recipe to add
  * @return 
  */
 export function addRecipe(recipe) {
@@ -21,12 +21,3 @@ export function addRecipe(recipe) {
     dispatch(add(recipe));
   };
 }
-
-/*
-    const recipe = {
-        id: uuidv1(),
-        name: "Shrimp and Chorizo Paella",
-
-    }; 
-
-    */
