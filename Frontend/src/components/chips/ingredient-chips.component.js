ce5f3f5c8096bcdb2f768e15e6083e9447ef405b
Pranav Gaikwad <gaikwad.pranav@gmail.com>
2019-03-13 21:05:48 -0400

    added chips to show ingredients on the recipe card

---
+++ Frontend/src/components/chips/ingredient-chips.component.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Chip from '@material-ui/core/Chip';
+import Paper from '@material-ui/core/Paper';
+import TagFacesIcon from '@material-ui/icons/TagFaces';
+
+class IngredientChipsComponent extends React.Component {
+
+  render() {
+    const { classes, ingredients, handleDelete } = this.props;
+
+    return ( 
+      <div>
+        {
+          ingredients.map(data => {
+            return (
+              <Chip
+                key={data.id}
+                label={data.name}
+                onDelete={(handleDelete !== undefined) ? () => handleDelete(data) : undefined}
+                className={classes.chip}
+              />
+            );
+          })
+        }
+      </div>
+    );
+  }
+}
+
+IngredientChipsComponent.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+const styles = theme => ({
+  root: {
+    display: 'flex',
+    justifyContent: 'center',
+    flexWrap: 'wrap',
+    padding: theme.spacing.unit / 2,
+  },
+  chip: {
+    margin: theme.spacing.unit / 2,
+  },
+});
+
+export default withStyles(styles)(IngredientChipsComponent);
