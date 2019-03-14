d745317a27a79a221e57bac7014c76cba2599dbb
Pranav Gaikwad <gaikwad.pranav@gmail.com>
2019-03-13 21:04:41 -0400

    updated

--- Frontend/src/components/dialogs/add-recipe-dialog.component.js
+++ Frontend/src/components/dialogs/add-recipe-dialog.component.js
@@ -1,13 +1,15 @@
+import { connect } from 'react-redux';
 import React, { Component } from 'react';
 import Button from '@material-ui/core/Button';
-import TextField from '@material-ui/core/TextField';
 import Dialog from '@material-ui/core/Dialog';
+import TextField from '@material-ui/core/TextField';
+import DialogTitle from '@material-ui/core/DialogTitle';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
-import DialogTitle from '@material-ui/core/DialogTitle';
 
-import EditRecipeFormComponent from '../forms/edit-recipe-form.component.js'
+import { sampleRecipe } from '../../utils/app.constants';
+import EditRecipeFormComponent from '../forms/edit-recipe-form.component';
 
 class AddRecipeDialog extends Component {
   constructor(props) {
@@ -15,95 +17,42 @@ class AddRecipeDialog extends Component {
 
     this.state = {
       recipe: {
-        name: '',
-        short_description: '',
-        description: '',
-        ingredients: [],
+        name: sampleRecipe.name,
+        short_description: sampleRecipe.shortDescription,
+        description: sampleRecipe.description,
+        ingredients: sampleRecipe.ingredients,
       },
     }
 
-    this.onFormChange = this.onFormChange.bind(this);
     this.onFormSubmit = this.onFormSubmit.bind(this);
   }
   
   onFormSubmit(e, recipe, nextFunction) {
-    this.setState({
-      recipe: {
-        name: '',
-        short_description: '',
-        description: '',
-        ingredients: [],
-      },
-    });
-
-    console.log(recipe);
-
     // callback from parent component
     nextFunction(e, recipe);
-  }
-
-  onFormChange(e) {
-    e.preventDefault();
-
-    const { target } = e;
-
-    switch (target.id) {
-      case 'title':
-        this.setState({
-          recipe: {
-            ...this.state.recipe,
-            name: target.value,
-          }
-        });
-        break;
-      case 'short_description':
-        this.setState({
-          recipe: {
-            ...this.state.recipe,
-            short_description: target.value,
-          }
-        });
-        break;
-      case 'description':
-        this.setState({
-          recipe: {
-            ...this.state.recipe,
-            description: target.value,
-          }
-        });
-        break;
-    }
-  }
-
-  componentDidMount() {
-    const { recipe } = this.props;
-
-    if (recipe !== undefined && recipe !== null && recipe) {
-      this.setState({
-        recipe: recipe,
-      });
-    }
   }
 
   render() {
     const { 
       open, 
+      recipe,
       onClose,
       onSubmit,
+      onFormChange,
+      onIngredientAdded,
+      onIngredientDeleted,
     } = this.props;
-
-    const { 
-      recipe,
-    } = this.state;
-
+  
     return (
-      <Dialog
-        open={open}
-        aria-labelledby="form-dialog-title"
-      >
+      <Dialog open={open} aria-labelledby="form-dialog-title">
         <DialogTitle id="form-dialog-title">Recipe</DialogTitle>
         <DialogContent>
-          <EditRecipeFormComponent recipe={recipe} onFormChange={this.onFormChange}/>
+          <EditRecipeFormComponent 
+            recipe={recipe} 
+            onFormChange={onFormChange} 
+            onIngredientAdded={onIngredientAdded} 
+            onIngredientDeleted={onIngredientDeleted}
+          />
         </DialogContent>
         <DialogActions>
           <Button onClick={onClose} color="secondary">
@@ -118,4 +67,10 @@ class AddRecipeDialog extends Component {
   }
 }
 
-export default AddRecipeDialog;
+const mapStateToProps = state => ({
+  allIngredients: state.ingredientsReducer.ingredients,
+});
+
+const mapDispatchToProps = dispatch => ({});
+
+export default connect(mapStateToProps, mapDispatchToProps)(AddRecipeDialog);
