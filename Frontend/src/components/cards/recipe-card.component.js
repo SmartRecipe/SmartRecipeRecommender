39411ab89ae9381c49bf9e22f8efe6cfb8dd0dd1
Pranav Gaikwad <gaikwad.pranav@gmail.com>
2019-03-15 18:27:54 -0400

    removed unused imports

--- Frontend/src/components/cards/recipe-card.component.js
+++ Frontend/src/components/cards/recipe-card.component.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Card from '@material-ui/core/Card';
-import Avatar from '@material-ui/core/Avatar';
 import Divider from '@material-ui/core/Divider';
 import { withStyles } from '@material-ui/core/styles';
 import IconButton from '@material-ui/core/IconButton';
