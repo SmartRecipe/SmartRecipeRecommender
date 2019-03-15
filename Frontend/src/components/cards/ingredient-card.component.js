e77e454bd0c2aba14ca49433239304c877a73a4a
Pranav Gaikwad <gaikwad.pranav@gmail.com>
2019-03-15 18:02:00 -0400

    added avatar for ingredient card

--- Frontend/src/components/cards/ingredient-card.component.js
+++ Frontend/src/components/cards/ingredient-card.component.js
@@ -1,22 +1,52 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import Card from '@material-ui/core/Card';
+import Avatar from '@material-ui/core/Avatar';
 import { withStyles } from '@material-ui/core/styles';
 import IconButton from '@material-ui/core/IconButton';
-import Typography from '@material-ui/core/Typography';
+import CardHeader from '@material-ui/core/CardHeader';
 import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
 
+import {
+  red, blue,
+  pink, cyan,
+  teal, lime,
+  green, brown,
+  orange, purple,
+  yellow, lightGreen,
+  lightBlue, deepPurple, amber,
+} from '@material-ui/core/colors';
 
 import EditIcon from '@material-ui/icons/Edit';
 import DeleteIcon from '@material-ui/icons/Delete';
+
+const avatarColors = [
+  red[500], blue[500], pink[500], cyan[500], lightBlue[900],
+  teal[500], lime[900], green[500], orange[500], purple[500],
+  brown[500], deepPurple[500], yellow[900], lightGreen[900], amber[500]
+];
 
 /**
  * Generic card component to show an ingredient
  */
 class IngredientCardComponent extends Component {
-  render(){
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      avatarColor: red[500],
+    }
+
+    this.avatarColor = red[500];
+  }
+
+  componentDidMount() {
+    this.setState({
+      avatarColor: avatarColors[Math.floor(Math.random()*avatarColors.length)],
+    });
+  }
+
+  render(){
     const {
       classes,
       ingredient,
@@ -31,17 +61,24 @@ class IngredientCardComponent extends Component {
     } = ingredient;
 
     const description = `${qty} ${unit}`;
-    
+
+    let avatar = 'R';
+
+    if (title) {
+      avatar = title[0];
+    }
+
     return (
       <Card className={classes.card}>
-        <CardContent>
-          <Typography variant="title" component="p">
-            {title}        
-          </Typography>
-          <Typography component="h5">
-            {description}
-          </Typography>
-        </CardContent>
+        <CardHeader
+          avatar={
+            <Avatar aria-label="Ingredient" style={{ backgroundColor: this.state.avatarColor }}>
+              {avatar}
+            </Avatar>
+          }
+          title={ title }
+          subheader={ description }
+        />
 
         <CardActions className={classes.actions} disableActionSpacing>
           <IconButton aria-label="Edit" onClick={() => onEditButtonClicked(id)}>
