bc537f1d50dae70c48935aa7bffded1f422c1885
Pranav Gaikwad <gaikwad.pranav@gmail.com>
2019-03-15 18:28:35 -0400

    added Avatar

--- Frontend/src/components/cards/ingredient-card.component.js
+++ Frontend/src/components/cards/ingredient-card.component.js
@@ -2,7 +2,6 @@ import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import Card from '@material-ui/core/Card';
 import Menu from '@material-ui/core/Menu';
-import Avatar from '@material-ui/core/Avatar';
 import MenuItem from '@material-ui/core/MenuItem';
 import { withStyles } from '@material-ui/core/styles';
 import IconButton from '@material-ui/core/IconButton';
