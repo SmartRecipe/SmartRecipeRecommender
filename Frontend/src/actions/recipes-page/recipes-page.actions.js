06fa34c30de66ef2ab4e7ca2f9af86b640f21e05
Pranav Gaikwad <gaikwad.pranav@gmail.com>
2019-03-23 10:07:19 -0400

    added getRecipes action to retrieve list of recipes

--- Frontend/src/actions/recipes-page/recipes-page.actions.js
+++ Frontend/src/actions/recipes-page/recipes-page.actions.js
@@ -1,5 +1,5 @@
-import uuidv1 from 'uuid/v1';
-
+import { apiProxy } from '../../utils/api-proxy.service';
+import { apiConstants } from '../../utils/app.constants';
 import { actionsRecipes } from '../../utils/app.constants';
 
 /**
@@ -8,23 +8,55 @@ import { actionsRecipes } from '../../utils/app.constants';
  * @return 
  */
 export function addRecipe(recipe) {
-  function add(recipe) {
-    let id = uuidv1();
-
-
-    if (recipe.id) {
-      id = recipe.id;
-    }
-
-    recipe = {
-      ...recipe,
-      id: id,
-    }
-
-    return { type: actionsRecipes.add, recipe: recipe };
+  function get(recipes) {
+    return { type: actionsRecipes.get, recipes };
   }
 
   return (dispatch) => {
-    dispatch(add(recipe));
+    if (recipe.id) {
+      apiProxy.put(`${apiConstants.base_url}${apiConstants.recipes}${recipe.id}`, recipe, '123')
+      .then((response) => {
+        return apiProxy.get(`${apiConstants.base_url}${apiConstants.recipes}`, '123');
+      })
+      .then((response) => {
+        console.log(response);
+        dispatch(get(response));      
+      })
+      .catch((e) => { // eslint-disable-line
+        console.log('error getting ingredients', e);
+      })
+    } else {
+      apiProxy.post(`${apiConstants.base_url}${apiConstants.recipes}`, recipe, '123')
+      .then((response) => {
+        return apiProxy.get(`${apiConstants.base_url}${apiConstants.recipes}`, '123');
+      })
+      .then((response) => {
+        dispatch(get(response));      
+      })
+      .catch((e) => { // eslint-disable-line
+        console.log('error getting ingredients', e);
+      })
+    };
+  };
+}
+
+/**
+ * Get list of recipes
+ * @return {[Object]} List of recipes
+ */
+export function getRecipes() {
+  function get(recipes) {
+    return { type: actionsRecipes.get, recipes };
+  }
+
+  return (dispatch) => {
+    apiProxy.get(`${apiConstants.base_url}${apiConstants.recipes}`, '123')
+    .then((response) => {
+      console.log('recipes : ', response);
+      dispatch(get(response));
+    })
+    .catch((e) => { // eslint-disable-line
+      console.log('error getting recipes', e);
+    })
   };
 }
