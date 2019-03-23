cced3c49a7baed2b4f48d689c8323ce060c344b0
Pranav Gaikwad <gaikwad.pranav@gmail.com>
2019-03-23 14:37:49 -0400

    added form submit actions in parent component

--- Frontend/src/components/forms/sign-on.component.js
+++ Frontend/src/components/forms/sign-on.component.js
@@ -7,6 +7,7 @@ import InputLabel from '@material-ui/core/InputLabel';
 import Typography from '@material-ui/core/Typography';
 import FormControl from '@material-ui/core/FormControl';
 import withStyles from '@material-ui/core/styles/withStyles';
+import CircularProgress from '@material-ui/core/CircularProgress';
 
 import { appConstants } from '../../utils/app.constants';
 
@@ -14,14 +15,53 @@ import { appConstants } from '../../utils/app.constants';
  * Generic form component used as sign in / sign up form
  */
 class UserSignOnComponent extends Component {
+  
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      password2: '',
+    };
+
+    this.onFormSubmit = this.onFormSubmit.bind(this);
+    this.onPassword2Changed = this.onPassword2Changed.bind(this);
+  }
+
+  onFormSubmit(e, nextFunction) {
+    const { password2 } = this.state;
+    const { user, isSignUp } = this.props;
+    const { password } = user;
+
+    if ((password === password2) || !isSignUp) {
+      nextFunction(e);
+    } else {
+
+    }
+  }
+
+  onPassword2Changed(e) {
+    this.setState({
+      password2: e.target.value,
+    });
+  }
+
   render() {
-    const { 
+    const {
+      user,
       classes,
       isSignUp, // trigger to change the form into a sign up component
       onSubmit, // submit function passed from parent component
+      onFormChange,
       onToggleClicked, // helps toggle between sign in and sign up forms
       signInButtonText, 
+      isSignInFailed,
+      isSignInPending,  
+      isSignInSuccess,
     } = this.props;
+
+    const { email, password } = user; 
+
+    const { password2 } = this.state;
 
     return (
       <div>
@@ -29,20 +69,20 @@ class UserSignOnComponent extends Component {
           <Typography component="h1" variant="h5">
             {appConstants.appTitle}
           </Typography>
-          <form className={classes.form} onSubmit={onSubmit}>
+          <form name="signOn" className={classes.form} onSubmit={(e) => this.onFormSubmit(e, onSubmit)}>
             <FormControl margin="normal" required fullWidth>
               <InputLabel htmlFor="email">Email Address</InputLabel>
-              <Input id="email" name="email" autoComplete="email" autoFocus />
+              <Input id="email" name="email" value={email} autoComplete="email" autoFocus onChange={onFormChange}/>
             </FormControl>
             <FormControl margin="normal" required fullWidth>
               <InputLabel htmlFor="password">Password</InputLabel>
-              <Input name="password" type="password" id="password" autoComplete="current-password" />
+              <Input name="password" type="password" value={password} id="password" autoComplete="current-password" onChange={onFormChange}/>
             </FormControl>
             {
               isSignUp &&
               <FormControl margin="normal" required fullWidth>
-                <InputLabel htmlFor="password">Confirm Password</InputLabel>
-                <Input name="password" type="password" id="password" />
+                <InputLabel htmlFor="password2">Confirm Password</InputLabel>
+                <Input name="password2" type="password" value={password2} id="password" onChange={this.onPassword2Changed}/>
               </FormControl>
             }
             {
@@ -53,9 +93,16 @@ class UserSignOnComponent extends Component {
               !isSignUp &&
               <Button fullWidth color="primary" onClick={onToggleClicked} className={classes.button}>Don't have an account ?</Button>
             }
-            <Button type="submit" fullWidth variant="contained" color="primary" className={classes.submit}>
-              {signInButtonText}
-            </Button>
+            {
+              !isSignInPending &&
+              <Button type="submit" name="signOnButton" fullWidth variant="contained" color="primary" className={classes.submit}>
+                {signInButtonText}
+              </Button>
+            }
+            {
+              isSignInPending &&
+              <CircularProgress className={classes.progress} />
+            }   
           </form>
         </Paper>
       </div>
@@ -87,6 +134,11 @@ const styles = theme => ({
   },
   button: {
     // margin: theme.spacing.unit,
+  },
+  progress: {
+    left: '50%',
+    marginLeft: -20,
+    position: 'relative',
   },
 });
 
