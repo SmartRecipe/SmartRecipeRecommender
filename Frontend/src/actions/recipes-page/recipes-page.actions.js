8a6c02a6b3fd53068ff619809b385b9f1686a2a4
Pranav Gaikwad <gaikwad.pranav@gmail.com>
2019-03-24 10:29:55 -0400

    added action to delete a recipe

--- Frontend/src/actions/recipes-page/recipes-page.actions.js
+++ Frontend/src/actions/recipes-page/recipes-page.actions.js
@@ -3,16 +3,35 @@ import { apiConstants } from '../../utils/app.constants';
 import { actionsRecipes } from '../../utils/app.constants';
 
 /**
- * Adding new recipe to the redux store
+ * Sets request as pending
+ */
+function setRequestPending() {
+  return { type: actionsRecipes.pending };
+}
+
+/**
+ * Sets request as failed
+ */
+function setRequestFailed() {
+  return { type: actionsRecipes.failed };
+}
+
+/**
+ * Populates redux store with list of recipes obtained from backend
+ * @param  {[Object]} recipes List of recipes
+ */
+function get(recipes) {
+  return { type: actionsRecipes.get, recipes };
+}
+
+/**
+ * Adds new recipe to the redux store
  * @param  {Object} recipe    Recipe to add
  * @return 
  */
 export function addRecipe(recipe) {
-  function get(recipes) {
-    return { type: actionsRecipes.get, recipes };
-  }
-
   return (dispatch) => {
+    dispatch(setRequestPending());
     if (recipe.id) {
       apiProxy.put(`${apiConstants.baseUrl}${apiConstants.recipes}${recipe.id}`, recipe, '123')
       .then((response) => {
@@ -22,6 +41,7 @@ export function addRecipe(recipe) {
         dispatch(get(response));      
       })
       .catch((e) => { // eslint-disable-line
+        dispatch(setRequestFailed());
         console.log('error getting ingredients', e);
       })
     } else {
@@ -33,6 +53,7 @@ export function addRecipe(recipe) {
         dispatch(get(response));      
       })
       .catch((e) => { // eslint-disable-line
+        dispatch(setRequestFailed());
         console.log('error getting ingredients', e);
       })
     };
@@ -44,17 +65,39 @@ export function addRecipe(recipe) {
  * @return {[Object]} List of recipes
  */
 export function getRecipes() {
-  function get(recipes) {
-    return { type: actionsRecipes.get, recipes };
-  }
-
   return (dispatch) => {
+    dispatch(setRequestPending());
+
     apiProxy.get(`${apiConstants.baseUrl}${apiConstants.recipes}`, '123')
     .then((response) => {
       dispatch(get(response));
     })
     .catch((e) => { // eslint-disable-line
+      dispatch(setRequestFailed());
       console.log('error getting recipes', e);
+    })
+  };
+}
+
+
+/**
+ * Delete a recipe
+ * @param {Object} recipe 
+ */
+export function deleteRecipe(id) {
+  return (dispatch) => {
+    dispatch(setRequestPending());
+
+    apiProxy.delete(`${apiConstants.baseUrl}${apiConstants.recipes}/${id}`, '123')
+    .then((response) => {
+      return apiProxy.get(`${apiConstants.baseUrl}${apiConstants.recipes}`, '123');
+    })
+    .then((response) => {
+      dispatch(get(response));      
+    })
+    .catch((e) => { // eslint-disable-line
+      dispatch(setRequestFailed());
+      console.log('Error deleting ingredient', e);
     })
   };
 }
