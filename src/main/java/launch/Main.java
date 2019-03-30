738ca7f3c3d3e9b2615fedb3a25f52c835668fc4
Jamie <waffleman566@gmail.com>
2019-03-30 13:00:11 -0400

    Added some missing dependencies and changed base directory for web application to Frontend/public/

--- src/main/java/launch/Main.java
+++ src/main/java/launch/Main.java
@@ -17,7 +17,7 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
 
-        String webappDirLocation = "Frontend/src/";
+        String webappDirLocation = "Frontend/public/";
         Tomcat tomcat = new Tomcat();
 
         //The port that we should run on can be set into an environment variable
