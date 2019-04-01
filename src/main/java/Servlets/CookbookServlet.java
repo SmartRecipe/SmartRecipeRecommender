eaf96765ccace88fad799c737c7f64c7f4f5f8ab
Pranav Gaikwad <gaikwad.pranav@gmail.com>
2019-04-01 19:10:24 -0400

    added URL to cookbook servlet

--- src/main/java/Servlets/CookbookServlet.java
+++ src/main/java/Servlets/CookbookServlet.java
@@ -14,11 +14,16 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.annotation.WebServlet;
 
 /**
  *
  * @author soup
  */
+@WebServlet(
+        name = "CookbookServlet",
+        urlPatterns = {"/cookbook"}
+    )
 public class CookbookServlet extends HttpServlet {
 	
     /**
